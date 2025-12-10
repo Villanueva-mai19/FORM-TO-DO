aef2b7ca1347bccc2e338c827358f1b3add68ab1
Villanueva-mai19 <michaels_pulido@soy.sena.edu.co>
2025-12-09 19:46:40 -0500

    subiendo proyecto

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { getTasks, createTask, updateTask, deleteTask } from "../api";
+import { getTasks, createTask, updateTask, deleteTask } from "../api/Index.jsx";
 import TodoForm from "../components/TodoForm";
 import TodoList from "../components/TodoList";
 import SearchBar from "../components/SearchBar";
